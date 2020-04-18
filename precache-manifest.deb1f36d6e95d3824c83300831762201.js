03fb32129499b2a034bc7d66fb237f7e0b73bc0c
Rajat-Pratap <rajat789pratap@gmail.com>
2020-04-18 15:00:07 +0530

    Updates

---
+++ precache-manifest.deb1f36d6e95d3824c83300831762201.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff75d8c198294d542a1587fd39419405",
+    "url": "/React-Todo-App/index.html"
+  },
+  {
+    "revision": "393dcfab121b839c05d9",
+    "url": "/React-Todo-App/static/css/main.04dd7562.chunk.css"
+  },
+  {
+    "revision": "0ef0ef91a70f56718b01",
+    "url": "/React-Todo-App/static/js/2.61421785.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Todo-App/static/js/2.61421785.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "393dcfab121b839c05d9",
+    "url": "/React-Todo-App/static/js/main.d1bcc516.chunk.js"
+  },
+  {
+    "revision": "9335e855d3c2bde28ad0",
+    "url": "/React-Todo-App/static/js/runtime-main.b7671be5.js"
+  }
+]);
